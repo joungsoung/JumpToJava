ed83d70aa80035a51fbbcdb0b1f52f32338b8b43
joungsoung <joungsoung@gmail.com>
2015-04-03 02:16:59 +0900

    TestForEach.java 추가

---
+++ JumpTojava/src/com/js/For/TestForEach.java
@@ -0,0 +1,11 @@
+package com.js.For;
+
+public class TestForEach {
+	public static void main(String[] args) {
+		String[] number = {"one", "two", "three"};
+		
+		for (String i : number) {
+			System.out.println(i);
+		}
+	}
+}
