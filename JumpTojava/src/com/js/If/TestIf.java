4174d9b658758d5a81ba22ff735d26a5a413c2db
joungsoung <joungsoung@gmail.com>
2015-04-03 02:17:26 +0900

    TestIf.java 추가

---
+++ JumpTojava/src/com/js/If/TestIf.java
@@ -0,0 +1,13 @@
+package com.js.If;
+
+public class TestIf {
+	public static void main(String[] args) {
+		int money = 2000;
+		
+		if ( money >= 3000) {
+			System.out.println("택시를 타고 가라");
+		} else {
+			System.out.println("걸어 가라");
+		}
+	}
+}
