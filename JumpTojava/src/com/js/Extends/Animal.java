db334ec49acb5da5ef59b3be65adf71e028cc89d
joungsoung <joungsoung@gmail.com>
2015-04-03 14:25:39 +0900

    extemds (상속. 추가)

---
+++ JumpTojava/src/com/js/Extends/Animal.java
@@ -0,0 +1,9 @@
+package com.js.Extends;
+
+public class Animal {
+	String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ JumpTojava/src/com/js/Extends/HouseDog.java
@@ -0,0 +1,12 @@
+package com.js.Extends;
+
+public class HouseDog extends Dog {
+	public void sleep() {
+		System.out.println(this.name+" zzz in house");
+	}
+	public static void main(String[] args) {
+		HouseDog houseDog = new HouseDog();
+		houseDog.setName("happy");
+		houseDog.sleep();
+	}
+}
