3121212297317faaa80fc3c364183077603e562a
joungsoung <joungsoung@gmail.com>
2015-04-07 16:34:27 +0900

    GuGuDan.java 추가 (구구단 출력하기)

---
+++ JumpTojava/src/com/js/Exercise/GuGuDan.java
@@ -0,0 +1,12 @@
+package com.js.Exercise;
+
+public class GuGuDan {
+	public static void main(String[] args) {
+		for (int a = 2; a < 10; a++) {
+			for (int b = 1; b < 10; b++) {
+				System.out.println(a + "*" + b + "=" + a * b);
+			}
+			System.out.println();
+		}
+	}
+}
