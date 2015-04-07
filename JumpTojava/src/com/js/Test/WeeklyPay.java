1ed0ef8d772a1a7c5c003589b89967e4ee5aa3f9
joungsoung <joungsoung@gmail.com>
2015-04-07 16:32:14 +0900

    no message

---
+++ JumpTojava/src/com/js/Test/WeeklyPay.java
@@ -0,0 +1,46 @@
+package com.js.Test;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class WeeklyPay {
+	String name;
+	int workTime;
+	int hourly;
+	int overTime;
+	int weekly;
+	
+	public WeeklyPay(String name, int workTime, int hourly) {
+		this.name = name;
+		this.workTime = workTime;
+		this.hourly = hourly;
+		
+		if (this.workTime > 40) {
+			this.overTime = this.workTime-40;
+			this.workTime = this.workTime-this.overTime;
+		}
+		this.weekly = (int)((this.workTime*this.hourly)+((this.hourly*1.5)*this.overTime));
+	}
+	public String payMent() {
+		DecimalFormat df = new DecimalFormat("#,###");	//천단위 콤마 찍기
+		return df.format(this.weekly);
+	}
+	
+	public static void main(String[] args) {
+		String name;
+		int workTime;
+		int hourly;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("이름을 입력하세요: ");
+		name = sc.nextLine();
+		System.out.print("근무시간을 입력하세요: ");
+		workTime = sc.nextInt();
+		System.out.print("시급을 입력하세요: ");
+		hourly = sc.nextInt();
+		sc.close();
+		
+		WeeklyPay wp = new WeeklyPay(name, workTime, hourly);
+		System.out.println(name+"의 주급: "+wp.payMent()+"원");
+	}
+}
