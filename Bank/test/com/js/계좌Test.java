06c7efafbd5496398db37c10a82bcd17853cf250
JoungSoung <joungsoung@gmail.com>
2015-04-17 19:24:15 +0900

    Bank프래젝트 의 TDD 파일

---
+++ Bank/test/com/js/계좌Test.java
@@ -0,0 +1,73 @@
+package com.js;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class °èÁÂTest {
+	
+	/*@Test
+	public void shouldHaveZero() {
+		°èÁÂ account = new °èÁÂ();
+		double current = account.getÀÜ¾×();
+		assertEquals(0, current, 0.00001);
+	}*/
+	
+	/*@Test
+	public void shouldAddMoney() {
+		°èÁÂ account = new °èÁÂ();
+		account.ÀÔ±İ(100);
+		assertEquals(100, account.getÀÜ¾×(), 0.00001);
+	}*/
+	
+	/*@Test
+	public void shouldSubMoney() {
+		°èÁÂ account = new °èÁÂ();
+		account.ÀÔ±İ(100);
+		account.Ãâ±İ(40);
+		assertEquals(60, account.getÀÜ¾×(), 0.00001);
+	}*/
+	
+	/*@Test(expected = IllegalArgumentException.class)
+	public void shouldHaveNoMoney() {
+		°èÁÂ account = new °èÁÂ();
+		account.ÀÔ±İ(100);
+		account.Ãâ±İ(140);
+	}*/
+	
+	/*@Test
+	public void shouldOneorTwo() {
+		ÀÔ·Â input = new ÀÔ·Â();
+		assertEquals(100, input.Àü´Ş("1", 100), 0.00001);
+	}*/
+	
+	/*@Test
+	public void inputBox() {
+		°èÁÂ account = new °èÁÂ();
+		Input input = new Input();
+		input.Ui();
+	}*/
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
